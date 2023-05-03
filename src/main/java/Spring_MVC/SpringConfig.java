21777e4d8b48e3b47853cb5a98e307dd8f0912eb
aktan322 <aktan3222@gmail.com>
2023-05-03 10:29:51 +0600

    first commit

---
+++ src/main/java/Spring_MVC/SpringConfig.java
@@ -0,0 +1,11 @@
+package Spring_MVC;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("Spring_MVC")
+@EnableWebMvc
+public class SpringConfig {
+}
